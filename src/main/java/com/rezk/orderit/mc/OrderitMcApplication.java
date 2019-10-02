abf4026884f7da63766f59177cac8ec26e35bc07
Fernando Rezk <fernandorezk@outlook.com>
2019-10-02 14:48:51 +0200

    Base project

---
+++ src/main/java/com/rezk/orderit/mc/OrderitMcApplication.java
@@ -0,0 +1,13 @@
+package com.rezk.orderit.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderitMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderitMcApplication.class, args);
+	}
+
+}
