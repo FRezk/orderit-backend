59e60da8cdecc6cf82c4d229f96721f20e19c96b
Fernando Rezk <fernandorezk@outlook.com>
2019-10-03 18:10:47 +0200

    Testing first domain class

--- src/main/java/com/rezk/orderit/mc/resources/CategoriaResource.java
+++ src/main/java/com/rezk/orderit/mc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.rezk.orderit.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rezk.orderit.mc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST ESTA FUNCIONANDO";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Network");
+		Categoria cat2 = new Categoria(2, "Office");
+		
+		List<Categoria> categories = new ArrayList<>();
+		categories.add(cat1);
+		categories.add(cat2);
+		
+		return categories;
 	}
 
 }
