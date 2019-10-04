acd8586cb1f3d5c3de45ad0596dafc54d91327e1
Fernando Rezk <fernandorezk@outlook.com>
2019-10-04 15:34:37 +0200

    Adding custom exception and handler for controllers

---
+++ src/main/java/com/rezk/orderit/mc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.rezk.orderit.mc.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.rezk.orderit.mc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+}
--- src/main/java/com/rezk/orderit/mc/services/CategoriaService.java
+++ src/main/java/com/rezk/orderit/mc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.rezk.orderit.mc.domain.Categoria;
 import com.rezk.orderit.mc.repositories.CategoriaRepository;
+import com.rezk.orderit.mc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado id: " + id + " tipo: " + Categoria.class.getSimpleName()));
 	}
 
 }
