2ec715b4d583e9916025ec6c8c08f864b9449547
Fernando Rezk <fernandorezk@outlook.com>
2019-10-10 18:00:54 +0200

    Including findAll operation for Categoria

---
+++ src/main/java/com/rezk/orderit/mc/dto/CategoriaDTO.java
@@ -0,0 +1,42 @@
+package com.rezk.orderit.mc.dto;
+
+import java.io.Serializable;
+
+import com.rezk.orderit.mc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {}
+
+	public CategoriaDTO(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public CategoriaDTO(Categoria categoria) {
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
--- src/main/java/com/rezk/orderit/mc/services/CategoriaService.java
+++ src/main/java/com/rezk/orderit/mc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rezk.orderit.mc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado id: " + id + " tipo: " + Categoria.class.getSimpleName()));
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 	public Categoria insert(Categoria categoria) {
