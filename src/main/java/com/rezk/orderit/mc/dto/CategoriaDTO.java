40e6bb210257f58a1830926451ca14cb991ff816
Fernando Rezk <fernandorezk@outlook.com>
2019-10-11 12:32:10 +0200

    Adding first Bean Validations

--- src/main/java/com/rezk/orderit/mc/dto/CategoriaDTO.java
+++ src/main/java/com/rezk/orderit/mc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.rezk.orderit.mc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.rezk.orderit.mc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Categoria needs a name!")
+	@Length(min = 5, max = 80, message = "Categoria name needs to be between 5 and 80 characters.")
 	private String nome;
 	
 	public CategoriaDTO() {}
@@ -21,6 +28,10 @@ public class CategoriaDTO implements Serializable {
 	public CategoriaDTO(Categoria categoria) {
 		this.id = categoria.getId();
 		this.nome = categoria.getNome();
+	}
+	
+	public Categoria fromDTO() {
+		return new Categoria(this.id, this.nome);
 	}
 
 	public Integer getId() {
--- src/main/java/com/rezk/orderit/mc/resources/CategoriaResource.java
+++ src/main/java/com/rezk/orderit/mc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -51,17 +53,19 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
-		categoria = service.insert(categoria);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
-				.toUri();
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoria) {
+		Categoria orm = categoria.fromDTO();
+		orm = service.insert(orm);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(orm.getId())
+				.toUri();	
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria categoria) {
+	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody CategoriaDTO categoria) {
 		categoria.setId(id);
-		categoria = service.update(categoria);
+		Categoria orm = categoria.fromDTO();
+		orm = service.update(orm);
 		return ResponseEntity.noContent().build();
 	}
 
